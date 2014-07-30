8dff309587b23a0c61c0201fbb88a0760de996ba
NickLydon <nickkell@gmail.com>
2014-07-30 19:33:51 +0100

    Refactoring
    Moved functions out of main and did some small refactoring

--- PropertyWebScraper/Program.fs
+++ PropertyWebScraper/Program.fs
@@ -11,7 +11,6 @@ open System.IO
 open System.Text
 open Newtonsoft.Json
 
-
 type Provider = XmlProvider<"stations-facilities.xml", false, true>
 type JsonProv = JsonProvider<"http://www.rightmove.co.uk/typeAhead/uknostreet/AL/DE/RN/EY/">
 type KeysProv = JsonProvider<"stationkeys.json">
@@ -32,8 +31,7 @@ let getStationKeys stationsOfInterest =
         loop sequence
 
     stationsOfInterest
-    |> List.ofSeq
-    |> List.map(fun station ->             
+    |> Seq.map(fun station ->             
         let typeaheadurl = "http://www.rightmove.co.uk/typeAhead/uknostreet/"
 
         let stationkey (station, response) =
@@ -43,27 +41,18 @@ let getStationKeys stationsOfInterest =
                 |> Seq.filter (fun r -> r.LocationIdentifier.ToLower().Contains("station"))
             if Seq.isEmpty stations then None else Some({ station = station; key = (Seq.head stations).LocationIdentifier.Replace("^", "%5E"); })
                 
-
-        let twoCharNameSplit = 
-            station.name 
-            |> windowed 2 
-            |> Seq.map System.String.Concat 
-            |> Seq.map(fun s -> s.ToUpper()) 
-            |> List.ofSeq
         let lookAheadUrls = 
-            let rec loop state url =
-                seq {
-                    match state with
-                    | [] -> failwith "supply some text"
-                    | h::[] -> 
-                        let newUrl = url + h + "/"
-                        yield newUrl                             
-                    | h::t -> 
-                        let newUrl = url + h + "/"
-                        yield newUrl 
-                        yield! loop t newUrl
-                }
-            loop twoCharNameSplit typeaheadurl
+            let twoCharNameSplit = 
+                station.name 
+                |> windowed 2 
+                |> Seq.map System.String.Concat 
+                |> Seq.map(fun s -> s.ToUpper()) 
+                |> List.ofSeq
+            twoCharNameSplit 
+            |> Seq.fold(fun (urls: string list, previous) current -> 
+                let newUrl = previous + current + "/" 
+                (newUrl::urls, newUrl)) ([], typeaheadurl)
+            |> fst
 
         lookAheadUrls
         |> Seq.map(fun url ->
@@ -84,43 +73,39 @@ let getStationKeys stationsOfInterest =
         |> stationkey
     )
 
-[<EntryPoint>]
-let main argv =     
-
-    let info = Provider.GetSample()
-    
-    let allLines = 
-        info.Stations.Stations
-        |> Array.map(fun s -> s.ServingLines.ServingLines)
-        |> Array.collect id
-        |> Set.ofArray
-
-    let stationsOfInterest = [ "Farringdon"; "Liverpool Street"; ]
-    let linesOfInterest = [ "Hammersmith & City"; "Metropolitan"; "Circle"; ]
-
-    let stations lines =
-        lines
-        |> Seq.map(fun line ->
-            info.Stations.Stations
-            |> Array.filter(fun s -> s.ServingLines.ServingLines |> Array.exists(fun a -> a = line))
-            |> Array.map(fun s -> 
-                let split = s.Placemark.Point.Coordinates.Split([| ',' |])
-                { name = s.Name; 
-                    serving = s.ServingLines.ServingLines; 
-                    point = (System.Decimal.Parse(split.[0]),  System.Decimal.Parse(split.[1])) })            
-        )
-        |> Seq.collect id
-        |> Set.ofSeq
-
-    
+let writeStationKeysToFile stations =         
     File.WriteAllText(
         "stationkeys.json",    
         Newtonsoft.Json.JsonConvert.SerializeObject(
-            getStationKeys (stations linesOfInterest)
-            |> List.filter Option.isSome
-            |> List.map Option.get
+            getStationKeys stations
+            |> Seq.filter Option.isSome
+            |> Seq.map Option.get
         )
     )
+
+    
+let stations lines =
+    let info = Provider.GetSample()
+
+    lines
+    |> Seq.map(fun line ->
+        info.Stations.Stations
+        |> Array.filter(fun s -> s.ServingLines.ServingLines |> Array.exists(fun a -> a = line))
+        |> Array.map(fun s -> 
+            let split = s.Placemark.Point.Coordinates.Split([| ',' |])
+            { name = s.Name; 
+                serving = s.ServingLines.ServingLines; 
+                point = (System.Decimal.Parse(split.[0]),  System.Decimal.Parse(split.[1])) })            
+    )
+    |> Seq.collect id
+    |> Set.ofSeq
+
+let linesOfInterest = [ "Hammersmith & City"; "Metropolitan"; "Circle"; ]
+
+[<EntryPoint>]
+let main argv =     
+
+
     
 //    let request = Http.Request("http://www.rightmove.co.uk/property-to-rent/find.html?searchType=RENT&locationIdentifier=STATION%5E3431&insId=4&radius=0.5&displayPropertyType=&minBedrooms=2&maxBedrooms=&minPrice=&maxPrice=2000&maxDaysSinceAdded=&retirement=&sortByPriceDescending=&_includeLetAgreed=on&primaryDisplayPropertyType=&secondaryDisplayPropertyType=&oldDisplayPropertyType=&oldPrimaryDisplayPropertyType=&letType=&letFurnishType=&houseFlatShare=false")
     
