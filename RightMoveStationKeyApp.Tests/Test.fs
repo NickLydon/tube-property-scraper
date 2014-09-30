76a42501b157770f3370a1f4f388a3c981aee5c5
Nicholas Lydon <nickkell@gmail.com>
2014-09-30 18:37:33 +0100

    changed the tests project to be for integration. fixed problem whereby rightmove had no key for location trying to find

--- RightMoveStationKeyApp.Tests/Test.fs
+++ RightMoveStationKeyApp.Tests/Test.fs
@@ -1,14 +1,15 @@
-﻿namespace RightMoveStationKeyApp.Tests
+﻿module RightMoveStationKeyApp.Integration.Tests
 
 open System
 open NUnit.Framework
 open FsUnit
 open FSharp.Data
-open RightMoveStationKeyApp
+open System.IO
+open XYZ.RightMoveStationKeyApp
 
-[<TestFixture>]
-type Test() = 
-    [<Test>]
-
-    member x.ShouldGiveEmptyKeyCollectionWhenGivenNoStations() = 
-        Seq.length (getStationKeys []) |> should equal 0
+[<Test>]
+let ``should give empty key collection when given no stations``() = 
+    let filename = "output.json"
+    run filename [| "Circle" |]
+    let contents = File.ReadAllText filename
+    contents |> should equal """[{"station":{"name":"Bow Road","serving":["Circle","District","Hammersmith & City"],"point":{"Item1":-0.024823746874528683,"Item2":51.526800254826064000}},"key":"STATION%5E1166"},{"station":{"name":"Embankment","serving":["Bakerloo","Circle","District","Northern"],"point":{"Item1":-0.122360215814178660,"Item2":51.507241796748396000}},"key":"STATION%5E3251"},{"station":{"name":"Euston Square","serving":["Circle","Hammersmith & City","Metropolitan"],"point":{"Item1":-0.135836140694320340,"Item2":51.525561006016105000}},"key":"STATION%5E3311"},{"station":{"name":"Farringdon","serving":["Circle","Hammersmith & City","Metropolitan"],"point":{"Item1":-0.105065254236880330,"Item2":51.520444754930790000}},"key":"STATION%5E3431"},{"station":{"name":"Tower Hill","serving":["Circle","District"],"point":{"Item1":-0.076353695174605170,"Item2":51.510065953884120000}},"key":"STATION%5E9272"}]"""
--- RightMoveStationKeyApp/Program.fs
+++ RightMoveStationKeyApp/Program.fs
@@ -1,55 +1,15 @@
 ﻿// Learn more about F# at http://fsharp.net
-open RightMoveStationKeyApp
+// See the 'F# Tutorial' project for more help.
 open FSharp.Data
 open System
 open System.IO
 open CommonTypes
 open Newtonsoft.Json
-
-type TflProvider = XmlProvider<"stations-facilities.xml", false, true>
-type JsonProv = JsonProvider<"http://www.rightmove.co.uk/typeAhead/uknostreet/AL/DE/RN/EY/">
-
-
-let writeStationKeysToFile stations =         
-    File.WriteAllText(
-        "stationkeys.json",    
-        Newtonsoft.Json.JsonConvert.SerializeObject(
-            getStationKeys stations
-            |> Seq.filter Option.isSome
-            |> Seq.map Option.get
-        )
-    )
-
-
-let stations lines =
-    let info = TflProvider.GetSample()
-
-    lines
-    |> Seq.map(fun line ->
-        info.Stations
-        |> Array.filter(fun s -> s.ServingLines |> Array.exists(fun a -> a = line))
-        |> Array.map(fun s -> 
-            let split = s.Placemark.Point.Coordinates.Split([| ',' |])
-            { name = s.Name; 
-                serving = s.ServingLines; 
-                point = (System.Decimal.Parse(split.[0]),  System.Decimal.Parse(split.[1])) })            
-    )
-    |> Seq.collect id
-    |> Set.ofSeq
+open XYZ.RightMoveStationKeyApp
 
 
 [<EntryPoint>]
 let main argv = 
-    let stationsOfInterest = 
-        let names = 
-            match argv with
-            | [| |] -> 
-                TflProvider.GetSample().Stations
-                |> Array.map (fun s -> s.Name)
-            | _ -> argv
-
-        names |> stations
-    
-    writeStationKeysToFile stationsOfInterest
+    run argv.[0] argv.[0..]
 
     0 // return an integer exit code
