4845c33ea74a4ca1bf05693e7739b045884ba4bb
Nicholas Lydon <nickkell@gmail.com>
2014-10-12 21:44:21 +0100

    added c sharp website project

---
+++ Website/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Ajax;
+using System.Threading.Tasks;
+
+namespace Website.Controllers
+{
+	public class HomeController : Controller
+	{
+		public async Task<ActionResult> Index ()
+		{
+			var mvcName = typeof(Controller).Assembly.GetName ();
+			var isMono = Type.GetType ("Mono.Runtime") != null;
+
+			ViewData ["Version"] = mvcName.Version.Major + "." + mvcName.Version.Minor;
+			ViewData ["Runtime"] = isMono ? "Mono" : ".NET";
+
+			return View ();
+		}
+	}
+}
